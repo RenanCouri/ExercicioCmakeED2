bfc799e8a6eaa42dec93d8bd0ccb9cf3a49ded49
RenanCouri <renan.couri@estudante.ufjf.br>
2024-11-08 01:27:23 -0300

    Adição dos arquivos base, com a build já feita.

---
+++ build/CMakeFiles/ExercicioCmakeED2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExercicioCmakeED2.dir/hello.cpp.o"
+  "CMakeFiles/ExercicioCmakeED2.dir/hello.cpp.o.d"
+  "ExercicioCmakeED2"
+  "ExercicioCmakeED2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExercicioCmakeED2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
